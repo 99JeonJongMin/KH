8ce28a219bf502c9e940bb99cdfbf6b9db846a95
99JeonJongMin <jnjjb05@naver.com>
2025-01-08 17:11:56 +0900

    first

---
+++ chap01/src/chap01/Hello.java
@@ -0,0 +1,13 @@
+package chap01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello JAVA");
+
+		System.out.print("Hello JAVA");
+		System.out.println("Hello JAVA");
+	}
+
+}
